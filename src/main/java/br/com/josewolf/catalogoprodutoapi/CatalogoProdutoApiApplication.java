dccf615ee76855cedee72d4ae42c33318e5a712b
José Wolf <wolf.jose89@gmail.com>
2025-05-06 23:04:26 -0300

    Criando o esqueleto do projeto

---
+++ src/main/java/br/com/josewolf/catalogoprodutoapi/CatalogoProdutoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.josewolf.catalogoprodutoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoProdutoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoProdutoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/josewolf/catalogoprodutoapi/CatalogoProdutoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.josewolf.catalogoprodutoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoProdutoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
